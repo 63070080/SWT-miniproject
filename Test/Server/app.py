6cc7a6eca5b24608dde18ca8d3dc0a06ce35b952
63070080 <63070080@kmitl.ac.th>
2023-04-09 20:44:12 +0700

    update

--- Test/Server/app.py
+++ Test/Server/app.py
@@ -1,22 +1,32 @@
-from flask import Flask, jsonify, request
+
 from pymongo import MongoClient
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = Flask(__name__)
 
-client = MongoClient("mongodb://mongo:27017/")
+client = MongoClient("mongodb://database:27017/")
 db = client["mydb"]
 collection = db["user"]
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get("/")
+async def getusers():
+    # items = collection.find({})
+    # return jsonify([{"name": item["name"]} for item in items])
 
-@app.route("/", methods=["GET"])
-def get_all_items():
-    items = collection.find({})
-    return jsonify([{"name": item["name"]} for item in items])
+    # name = request.json["name"]
+    # collection.insert_one({"name": name})
 
-@app.route("/", methods=["POST"])
-def create_item():
-    name = request.json["name"]
-    collection.insert_one({"name": name})
-    return jsonify({"message": "Item created successfully"})
+    return ""
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+
+@app.post("/process-image")
+async def process_image():
+    return "dd"
